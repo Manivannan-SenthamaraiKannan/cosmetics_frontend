4d9fdab2baffbf53e7c0ac12bbfa889a16d1c453
Manivannan-SenthamaraiKannan <sharvamanivannan@gmail.com>
2024-02-23 16:12:20 +0530

    Offer Carousel

--- myappp/src/App.js
+++ myappp/src/App.js
@@ -2,16 +2,19 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import AdBanner from "./Layout/AdBanner";
 import TopNav from "./Layout/TopNav";
+import Offer from "./Components/Offer/Offer";
 import Login from "./Authentication/Login";
 import Register from "./Authentication/Register";
 import Resetpassword from "./Authentication/ResetPassword";
 import Footer from "./Layout/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <AdBanner />
       <TopNav />
+      <Offer />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
